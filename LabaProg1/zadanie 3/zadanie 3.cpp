a85f89eaf17a06c45eb24950b9cf31c87b232f4d
KP9KBA <bazanova01@bk.ru>
2019-10-02 15:58:00 +0300

    LabaProg1
    Все задания в одном файле

---
+++ LabaProg1/zadanie 3/zadanie 3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Write a";
+	cin >> a;
+	cout << "Write b";
+	cin >> b;
+	int Cp = (a + b) / 2;
+	cout << "Cp = " << Cp;
+	return 0;
+
+}
---
+++ LabaProg1/zadanie2/zadanie2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float p = 3.14;
+	int d;
+	cout << "Write d";
+	cin >> d;
+	float L = p * d;
+	cout << "L = " << L << endl;
+	return 0;
+
+}
