93d0d70eb46240b3af0d90182f4d4e24c45fca5a
KP9KBA <bazanova01@bk.ru>
2019-10-02 16:10:44 +0300

    LabaProg2

---
+++ LabaProg2/2.1/2.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float x1, x2, y1, y2, AB;
+	cout << "Введите координаты точки A " << endl;
+	cin >> x1 >> y1;
+	cout << "Введите координаты точки B " << endl;
+	cin >> x2 >> y2;
+	AB = fabs(sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)));
+	cout << "Расстояние между точками равно " << AB << endl;
+
+	return 0;
+}
+
---
+++ LabaProg2/2.5/2.5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double x1, x2, x3, y1, y2, y3, P, S, AB, BC, AC, p;
+
+	cout << "Введите координаты точки А (х,y) " << endl;
+	cin >> x1 >> y1;
+	cout << "Введите координаты точки В (x,у)" << endl;
+	cin >> x2 >> y2;
+	cout << "Введите координаты точки C (х,y) " << endl;
+	cin >> x3 >> y3;
+	AB = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));//Длина отрезков
+	BC = sqrt(pow((x3 - x2), 2) + pow((y3 - y2), 2));
+	AC = sqrt(pow((x3 - x1), 2) + pow((y3 - y1), 2));
+	P = AC + BC + AB;
+	p = P / 2;
+	S = sqrt(p * (p - AB) * (p - BC) * (p - AC));//Формула Герона
+	cout << "Периметр прямоугольника равен " << P << endl;
+	cout << "Площадь прямоугольника равна " << S << endl;
+
+
+	return 0;
+}
