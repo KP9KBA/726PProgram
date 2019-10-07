12e5546125e7e09387261e075db1a0ec69167ecd
KP9KBA <bazanova01@bk.ru>
2019-10-08 01:02:26 +0300

    Задания 6 7

---
+++ Laba 3/3.7/3.7.cpp
@@ -0,0 +1,14 @@
+﻿#include <pch.h>
+#include<iostream>
+using namespace std;
+int main() {
+	double A, a, b;
+	cout << "Write A" << endl;
+	cin >> A;
+	a = A * A * A;//3
+	b = a * a;//6
+	A = b * b;//12
+	cout << "A = " << A * a << endl;//15
+
+	return 0;
+}
