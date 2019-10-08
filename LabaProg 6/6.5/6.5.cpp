cc000d96de1bc79cd99246c7a3a3250e9ea31868
KP9KBA <bazanova01@bk.ru>
2019-10-09 00:21:55 +0300

    Задание 5
    Добавила задание 5, но задания 3 еще нет

---
+++ LabaProg 6/6.5/6.5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int x, y; //x-god y-vek
+	cout << "Введите год" << endl;
+	cin >> x;
+	y = floor(x / 100) + 1;
+	cout << "Это " << y << " век" <<endl;
+
+
+	return 0;
+}
