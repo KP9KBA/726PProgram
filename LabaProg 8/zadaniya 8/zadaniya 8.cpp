43c7de56b7c0885cf7397bb3b9ab0f98a9d3d53f
KP9KBA <bazanova01@bk.ru>
2019-10-11 15:10:45 +0300

    Задание 1
    Лаба по програм 8

---
+++ LabaProg 8/zadaniya 8/zadaniya 8.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int A, B;
+	
+	//zadanie 1
+	cout << "Введите два числа" << endl;
+	cin >> A >> B;
+	if (A == B) {
+		A = 0; B = 0;
+		cout << "Новые значения переменных A и B " << endl << A << endl << B;
+	}
+	else {
+		if (A > B) {
+			B = A;
+			cout << "Новые значения переменных A и B " << endl << A << endl << B;
+		}
+		else {
+			A = B;
+			cout << "Новые значения переменных A и B" << endl << A << endl << B;
+		}
+	}
+
+
+	return 0;
+}
